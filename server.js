6ad7602c51406981875d6cf447faad89e7d6b640
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-04-12 13:21:06 +0700

    basic server.js complete

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+  }
+);
+
+app.use(express.static(__dirname));
+app.use(__dirname, express.static(__dirname));
+
+
+
+app.listen(PORT, function() {
+    console.log('Server is running on PORT:',PORT);
+});
+
